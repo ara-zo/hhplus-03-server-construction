fc16fb8145210c8a13375ba96359f1919aab8ac5
ara-zo <jo91kr@gmail.com>
2024-10-06 19:08:51 +0900

    :tada: Initial commit

---
+++ src/main/kotlin/io/hhplus/server_construction/Hhplus03ServerConstructionApplication.kt
@@ -0,0 +1,11 @@
+package io.hhplus.server_construction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Hhplus03ServerConstructionApplication
+
+fun main(args: Array<String>) {
+    runApplication<Hhplus03ServerConstructionApplication>(*args)
+}
---
+++ src/test/kotlin/io/hhplus/server_construction/Hhplus03ServerConstructionApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.hhplus.server_construction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Hhplus03ServerConstructionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
